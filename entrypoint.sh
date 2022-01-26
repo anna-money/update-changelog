3e185a15dd4c6707a4678a4437069cdf476bfde4
Ivan Fateev <ivan@anna.money>
2022-01-26 18:03:51 +0300

    some fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export CHANGELOG_FILE=$1
 export CHANGELOG_START_TAG=$2
 export CHANGELOG_TEMPLATE_FILE=$3
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+export SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-git config gitchangelog.template-path $SCRIPT_DIR/templates
+# git config gitchangelog.template-path "$SCRIPT_DIR/templates"
 python3 ./gitchangelog ${CHANGELOG_START_TAG}..HEAD > $CHANGELOG_FILE
