73102e750cab2b6004b65d09bb57e10bea837aa4
Ivan Fateev <ivan@anna.money>
2022-01-26 18:05:16 +0300

    Add error checking and debugging

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM alpine:3.10
 
 RUN apk update
 RUN apk add curl py3-pip git
-RUN curl -sSL https://raw.githubusercontent.com/vaab/gitchangelog/master/src/gitchangelog/gitchangelog.py > gitchangelog
-RUN chmod +x gitchangelog
+RUN curl -sSL https://raw.githubusercontent.com/vaab/gitchangelog/master/src/gitchangelog/gitchangelog.py > /usr/bin/gitchangelog
+RUN chmod +x /usr/bin/gitchangelog
 RUN pip3 install pystache
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
+set -x
+set -e
 export PYTHONIOENCODING=UTF-8
 export LC_CTYPE=en_US.UTF-8
 export CHANGELOG_FILE=$1
@@ -7,5 +9,5 @@ export CHANGELOG_TEMPLATE_FILE=$3
 
 export SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-# git config gitchangelog.template-path "$SCRIPT_DIR/templates"
-python3 ./gitchangelog ${CHANGELOG_START_TAG}..HEAD > $CHANGELOG_FILE
+git config gitchangelog.template-path "$SCRIPT_DIR/templates"
+python3 /usr/bin/gitchangelog ${CHANGELOG_START_TAG}..HEAD > $CHANGELOG_FILE
